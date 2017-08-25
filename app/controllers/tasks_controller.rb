1c81759e4a77757029f39dc537ad9a5034c80752
TYS5629 <yutahimi@ac.cyberhome.ne.jp>
2017-08-25 07:58:19 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
-    @tasks = Task.all.page(params[:page]).per(10)
+    if logged_in?
+      @user = current_user
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page]).per(10)
+    end
   end
   
   def show
@@ -13,7 +17,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に登録されました'
