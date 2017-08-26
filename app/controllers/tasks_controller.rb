b9232c32379889dd9d5c9e0fd34e6f20cba4ceea
TYS5629 <yutahimi@ac.cyberhome.ne.jp>
2017-08-26 13:58:34 +0000

    refix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :correct_user, only: [:edit, :update, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
